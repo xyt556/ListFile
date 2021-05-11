31e4464a3362293799bf5eabd76fa9182ecf69da
R*K <49977485+KOFighting@users.noreply.github.com>
2021-05-11 08:56:13 +0800

    Update ListFile.py

--- ListFile.py
+++ ListFile.py
@@ -5,6 +5,8 @@ import sys
     Author：jdr
     Version：2021-5-11
 '''
+
+#获取文件目录
 def get_path(path):
     flag = True         #设置标志位，True代表当前目录还有子目录
     dir_list = [path]       #初始化一个目录列表,代表当前目录的子目录有哪些
@@ -22,21 +24,23 @@ def get_path(path):
                 pass
         if len(dir_list) <= 0:
             flag = False
-    return file_list        
+    return file_list
 
+#判断文件后缀    
 def process_the_file(path,filetype):
     file_list = get_path(path)
     for file in file_list:
-        if file.endswith(filetype):     #判断文件后缀            
-            file = file.replace(path,'/')#去除根目录
-            print(file)
-            save_file(file)
-            
+        if filetype != '*':
+            if file.endswith('.'+filetype):     #判断文件后缀            
+                file = file.replace(path,'/')#去除根目录
+                print(file)
+                save_file(file)
         else:
             file = file.replace(path,'/')
             print(file)
             save_file(file)
 
+#存储数据
 def save_file(file):
     with open('file.txt','a')as f:
         f.write(file+'\n')
